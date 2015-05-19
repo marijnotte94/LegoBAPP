2fdb51f1055987f90a8cec911fa31c876fa7536d
Marijn Otte <marijnotte94@gmail.com>
2015-05-19 17:27:25 +0200

    added steps to break down lego

--- Share2PleyApp/src/com/share2pley/share2pleyapp/ClearActivity.java
+++ Share2PleyApp/src/com/share2pley/share2pleyapp/ClearActivity.java
@@ -1,12 +1,66 @@
 package com.share2pley.share2pleyapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.share2pley.share2pleyapp.Model.Instruction;
+import com.share2pley.share2pleyapp.Model.Set;
+import com.share2pley.share2pleyapp.Model.SetLab;
 
 public class ClearActivity extends Activity {
+	
+	private Set set;
+	private TextView message;
+	private Button nextButton;
+	private Instruction current;
+	private int index;
+	private int s = 0;
+	
+	
+	
+	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_clear);
+		
+		Intent i = getIntent();
+		Bundle b = i.getExtras();
+		if(b != null){
+			 s = b.getInt("SETNO");
+		}
+		
+		
+		set = SetLab.get().getSet(s);
+		current = set.getInstruction(0);
+		index = 0;
+		
+		message = (TextView)findViewById(R.id.message_instruction);
+		message.setTextColor(current.setForeGround(current.getColor()));
+		message.setText(current.toString());
+		
+		nextButton = (Button)findViewById(R.id.build_next);
+		nextButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if(set.hasNext(index)){
+					index ++;
+					current = set.getInstruction(index);
+					message.setTextColor(current.setForeGround(current.getColor()));
+					message.setBackgroundColor(current.setBackGround(current.getColor()));
+					message.setText(current.toString());
+				}
+				else{
+					message.setText("Well Done");
+				}
+			}
+		});
 	}
+	
+	
 }
---
+++ Share2PleyApp/src/com/share2pley/share2pleyapp/Model/SetLab.java
@@ -0,0 +1,82 @@
+package com.share2pley.share2pleyapp.Model;
+
+import java.util.ArrayList;
+
+public class SetLab {
+
+	private static Set set1; 
+	private static Set set2; 
+	private static Set set3;
+	private static Set set4;
+	private static Set set5;
+	private static Set set6;
+	private static Set set7;
+	private static Set set8;
+	private static Set set9;
+	private static Set set10;
+	private static Set set11;
+	private static Set set12;
+	private static Set set13;
+	private static Set set14;
+	private static Set set15;
+	private static Set set16;
+	
+	private static ArrayList<Set> sets;
+	private static SetLab sSetLab;
+	
+	
+	private SetLab(){
+		sets =  new ArrayList<Set>();
+		
+		set1 = new Set(4,4,65,33,33,11,12,12,12);
+		set2 = new Set(4,4,65,33,33,11,12,12,12);
+		set3 = new Set(4,4,65,33,33,11,12,12,12);
+		set4 = new Set(4,4,65,33,33,11,12,12,12);
+		set5 = new Set(4,4,65,33,33,11,12,12,12);
+		set6 = new Set(4,4,65,33,33,11,12,12,12);
+		set7 = new Set(4,4,65,33,33,11,12,12,12);
+		set8 = new Set(4,4,65,33,33,11,12,12,12);
+		set9 = new Set(4,4,65,33,33,11,12,12,12);
+		set10 = new Set(4,4,65,33,33,11,12,12,12);
+		set11 = new Set(4,4,65,33,33,11,12,12,12);
+		set12 = new Set(4,4,65,33,33,11,12,12,12);
+		set13 = new Set(4,4,65,33,33,11,12,12,12);
+		set14 = new Set(4,4,65,33,33,11,12,12,12);
+		set15 = new Set(4,4,65,33,33,11,12,12,12);
+		//correct, dont change!
+		set16 = new Set(6,19,13,9,19,171,204,48,4);
+		
+		sets.add(set1);
+		sets.add(set2);
+		sets.add(set3);
+		sets.add(set4);
+		sets.add(set5);
+		sets.add(set6);
+		sets.add(set7);
+		sets.add(set8);
+		sets.add(set9);
+		sets.add(set10);
+		sets.add(set11);
+		sets.add(set12);
+		sets.add(set13);
+		sets.add(set14);
+		sets.add(set15);
+		sets.add(set16);
+	}
+
+	public static SetLab get(){
+		if (sSetLab == null){
+			sSetLab = new SetLab();
+		}
+		return sSetLab;
+	}
+	
+	public static Set getSet(int index){
+		return sets.get(index); 
+	}
+	
+	public static int getAmount(){
+		return sets.size();
+	}
+
+}
