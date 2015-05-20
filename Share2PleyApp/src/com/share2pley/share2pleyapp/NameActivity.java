e6f8f114dfbcb2b29cc683ace0920052afbd443b
Richard <r.vink386@gmail.com>
2015-05-20 10:03:33 +0200

    Working Master

--- Share2PleyApp/src/com/share2pley/share2pleyapp/NameActivity.java
+++ Share2PleyApp/src/com/share2pley/share2pleyapp/NameActivity.java
@@ -1,5 +1,3 @@
-
-<<<<<<< HEAD
 package com.share2pley.share2pleyapp;
 
 import android.app.Activity;
@@ -27,7 +25,7 @@ public class NameActivity extends Activity{
 
 		mFirstName = (EditText)findViewById(R.id.edit_first_name);
 		mLastName = (EditText)findViewById(R.id.edit_last_name);
-		
+
 		getBaseContext().deleteDatabase("Persons");
 		spDb = new DBHelper(this);
 		try {
@@ -51,86 +49,19 @@ public class NameActivity extends Activity{
 			Log.i("NameActivity", e.toString());
 		}
 
-			mConfirmButton = (Button)findViewById(R.id.confirm_button);
-			mConfirmButton.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					if(isAlreadyFilled) {
-						spDb.updateContent(1, mFirstName.getText().toString(), mLastName.getText().toString(), 0);
-					}
-					spDb.insertPerson(mFirstName.getText().toString(), mLastName.getText().toString(), 0);
-					Intent i = new Intent(getBaseContext(), MenuActivity.class);
-					startActivity(i);
-					finish();
+		mConfirmButton = (Button)findViewById(R.id.confirm_button);
+		mConfirmButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if(isAlreadyFilled) {
+					spDb.updateContent(1, mFirstName.getText().toString(), mLastName.getText().toString(), 0);
 				}
-			});		
-		}
-
-	}
-=======
-import android.app.Activity;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.CursorIndexOutOfBoundsException;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-public class NameActivity extends Activity{
-	private DBHelper spDb;
-	Button mConfirmButton;
-	EditText mFirstName;
-	EditText mLastName;
-	private boolean isAlreadyFilled = false;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		//Setup the GUI with the corresponding XML file
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_name_fill);
-
-		mFirstName = (EditText)findViewById(R.id.edit_first_name);
-		mLastName = (EditText)findViewById(R.id.edit_last_name);
-		
-		getBaseContext().deleteDatabase("Persons");
-		spDb = new DBHelper(this);
-		try {
-			if(spDb.getData(1)!= null) {
-				isAlreadyFilled=true;
-				Cursor rs = spDb.getData(1);
-				rs.moveToFirst();
-				String firstName = rs.getString(rs.getColumnIndex(DBHelper.PERSONS_COLUMN_FIRSTNAME));
-				String lastName = rs.getString(rs.getColumnIndex(DBHelper.PERSONS_COLUMN_LASTNAME));
-
-				if(!rs.isClosed()) {
-					rs.close();
-				}
-
-				if(!firstName.equals(null) && !lastName.equals(null)) {
-					mFirstName.setText((CharSequence)firstName);
-					mLastName.setText((CharSequence)lastName);
-				}
+				spDb.insertPerson(mFirstName.getText().toString(), mLastName.getText().toString(), 0);
+				Intent i = new Intent(getBaseContext(), MenuActivity.class);
+				startActivity(i);
+				finish();
 			}
-		} catch (CursorIndexOutOfBoundsException e) {
-			Log.i("NameActivity", e.toString());
-		}
-
-			mConfirmButton = (Button)findViewById(R.id.confirm_button);
-			mConfirmButton.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					if(isAlreadyFilled) {
-						spDb.updateContent(1, mFirstName.getText().toString(), mLastName.getText().toString(), 0);
-					}
-					spDb.insertPerson(mFirstName.getText().toString(), mLastName.getText().toString(), 0);
-					Intent i = new Intent(getBaseContext(), MenuActivity.class);
-					startActivity(i);
-					finish();
-				}
-			});		
-		}
-
+		});		
 	}
->>>>>>> 7ab8b6b7ac9fcf1c61361ec08cf6b6f581d807c8
+
+}
--- Share2PleyApp/src/com/share2pley/share2pleyapp/StartActivity.java
+++ Share2PleyApp/src/com/share2pley/share2pleyapp/StartActivity.java
@@ -1,5 +1,3 @@
-
-<<<<<<< HEAD
 package com.share2pley.share2pleyapp;
 
 import android.app.Activity;
@@ -15,11 +13,11 @@ public class StartActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		
+
 		//Setup the GUI with the corresponding XML file
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
-		
+
 		/**
 		 * StartButton with new OnClickListener
 		 * Starts new NameActivity and finishes the StartActivity
@@ -34,37 +32,3 @@ public class StartActivity extends Activity {
 		});
 	}
 }
-=======
-import android.app.Activity;
-import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-public class StartActivity extends Activity {
-	Button mStartButton;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		
-		//Setup the GUI with the corresponding XML file
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_start);
-		
-		/**
-		 * StartButton with new OnClickListener
-		 * Starts new NameActivity and finishes the StartActivity
-		 */
-		mStartButton = (Button)findViewById(R.id.start_button);
-		mStartButton.setOnClickListener(new View.OnClickListener() {	
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(getBaseContext(), NameActivity.class);
-				startActivity(i);
-			}
-		});
-	}
-}
->>>>>>> 7ab8b6b7ac9fcf1c61361ec08cf6b6f581d807c8
