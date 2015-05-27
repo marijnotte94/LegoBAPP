c1b0802581ba1b5efdd94d9ae859dc945436b712
Marijn Otte <marijnotte94@gmail.com>
2015-05-27 16:33:58 +0200

    set 1 correct bricks

---
+++ Share2PleyApp/src/com/share2pley/share2pleyapp/Tests/TestSet.java
@@ -0,0 +1,28 @@
+package com.share2pley.share2pleyapp.Tests;
+
+import static org.junit.Assert.*;
+import junit.framework.TestSuite;
+
+import org.junit.Test;
+
+import com.share2pley.share2pleyapp.Model.Brick;
+import com.share2pley.share2pleyapp.Model.Set;
+
+public class TestSet extends TestSuite {
+	/*
+	Set set = new Set(5,12,33,55,22,11,12, 5, 18);
+	
+	
+	@Test
+	public void Test() {
+		assertEquals(set.hasNext(4), true);
+		assertEquals(set.hasPrevious(0),false);
+		assertEquals(set.hasPrevious(1), true);
+		for(int i = 0; i<set.getIntructions().size()-1; i++){
+			assertTrue(set.getInstruction(i).getAmount() <= 10);
+		}
+		
+		
+	}
+*/
+}
