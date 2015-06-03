8fcddd4298bbf90c2ded3e4d893e1077222f33ff
Richard <r.vink386@gmail.com>
2015-06-03 10:12:04 +0200

    fixed the import of java

--- Share2PleyApp/src/com/share2pley/share2pleyapp/Model/Set.java
+++ Share2PleyApp/src/com/share2pley/share2pleyapp/Model/Set.java
@@ -1,6 +1,5 @@
 package com.share2pley.share2pleyapp.Model;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 import android.content.Context;
@@ -10,38 +9,41 @@ import com.share2pley.share2pleyapp.R;
 
 public class Set {
 	public String mName;
-	private ArrayList<Brick> mBricks;
-	private Context mContext;
-	private TypedArray mImgs;
-	private TypedArray mFigurines;
+	private final ArrayList<Brick> mBricks;
+	private final Context mContext;
+	private final TypedArray mImgs;
+	private final TypedArray mFigurines;
 
-	public Set(String name, Context context){
+	public Set(String name, Context context) {
 		mName = name;
 		mContext = context;
 		mBricks = new ArrayList<Brick>();
 		mImgs = context.getResources().obtainTypedArray(R.array.models_imgs);
 		context.getResources().obtainTypedArray(R.array.models_imgs).recycle();
-		mFigurines = context.getResources().obtainTypedArray(R.array.figurines_imgs);
-		context.getResources().obtainTypedArray(R.array.figurines_imgs).recycle();
+		mFigurines = context.getResources().obtainTypedArray(
+				R.array.figurines_imgs);
+		context.getResources().obtainTypedArray(R.array.figurines_imgs)
+				.recycle();
 		addBricks();
 	}
 
-	//check if there is a next instruction
-	public boolean hasNext(int ins){
-		if(ins <= mBricks.size()-2){
+	// check if there is a next instruction
+	public boolean hasNext(int ins) {
+		if (ins <= mBricks.size() - 2) {
 			return true;
 		}
 		return false;
 	}
-	//check if there is a previous instruction
-	public boolean hasPrevious(int ins){
-		if(ins == 0){
+
+	// check if there is a previous instruction
+	public boolean hasPrevious(int ins) {
+		if (ins == 0) {
 			return false;
 		}
 		return true;
 	}
 
-	public void addBrick(int source, int amount){
+	public void addBrick(int source, int amount) {
 		mBricks.add(new Brick(source, amount, mContext));
 	}
 
@@ -49,41 +51,40 @@ public class Set {
 		return mBricks.get(index);
 	}
 
-	public void addBricks(){
+	public void addBricks() {
 		for (int i = 0; i < mImgs.length(); i++) {
-			addBrick(getModelImageResource(i),3);
+			addBrick(getModelImageResource(i), 3);
 		}
-	
-	
-	
-//		final R.drawable drawableResources = new R.drawable();
-//		final Class<R.drawable> c = R.drawable.class;
-//		final Field[] fields = c.getDeclaredFields();
-//
-//		for (int j = 0, max = fields.length; j < max; j++) {
-//			final int resourceId;
-//			try {
-//				resourceId = fields[j].getInt(drawableResources);
-//				String resourceName = mContext.getResources().getString(resourceId);
-//				if(resourceName.contains(mName)){
-//					int amount = Character.getNumericValue(resourceName.charAt(resourceName.length()-5));	 
-//					addBrick(resourceId, amount);
-//				}
-//			} catch (Exception e) {
-//				continue;
-//			}
-//		}
+
+		// final R.drawable drawableResources = new R.drawable();
+		// final Class<R.drawable> c = R.drawable.class;
+		// final Field[] fields = c.getDeclaredFields();
+		//
+		// for (int j = 0, max = fields.length; j < max; j++) {
+		// final int resourceId;
+		// try {
+		// resourceId = fields[j].getInt(drawableResources);
+		// String resourceName = mContext.getResources().getString(resourceId);
+		// if(resourceName.contains(mName)){
+		// int amount =
+		// Character.getNumericValue(resourceName.charAt(resourceName.length()-5));
+		// addBrick(resourceId, amount);
+		// }
+		// } catch (Exception e) {
+		// continue;
+		// }
+		// }
 	}
-	
-	public int getAmountBricks(){
+
+	public int getAmountBricks() {
 		return mBricks.size();
 	}
-	
+
 	public int getModelImageResource(int index) {
 		return mImgs.getResourceId(index, -1);
 	}
-	
+
 	public int getFigurineImageResource(int index) {
-		return mFigurines.getResourceId(index/2, -1);
+		return mFigurines.getResourceId(index / 2, -1);
 	}
 }
