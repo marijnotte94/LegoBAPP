e504dda1bbfbede6640f1fe263767100ac9c60b3
Richard <r.vink386@gmail.com>
2015-06-04 14:06:37 +0200

    added references

--- Share2PleyApp/src/com/share2pley/share2pleyapp/ReferenceActivity.java
+++ Share2PleyApp/src/com/share2pley/share2pleyapp/ReferenceActivity.java
@@ -1,9 +1,11 @@
 package com.share2pley.share2pleyapp;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -27,19 +29,25 @@ public class ReferenceActivity extends Activity {
 			public void onClick(View v) {
 				final String legoCityName = "com.lego.city.my_city";
 				Intent i = new Intent("com.lego.city.my_city");
-				if (i.equals(null)) {
+				try {
+					startActivity(i);
+					return;
+				} catch (ActivityNotFoundException e) {
+				}
+				try {
+					startActivity(new Intent(Intent.ACTION_VIEW, Uri
+							.parse("market://details?id=" + legoCityName)));
+				} catch (android.content.ActivityNotFoundException anfe) {
 					try {
-						startActivity(new Intent(Intent.ACTION_VIEW, Uri
-								.parse("market://details?id=" + legoCityName)));
-					} catch (android.content.ActivityNotFoundException anfe) {
 						startActivity(new Intent(
 								Intent.ACTION_VIEW,
 								Uri.parse("https://play.google.com/store/apps/details?id="
 										+ legoCityName)));
+					} catch (ActivityNotFoundException e) {
+						Log.e("TAG", "activity not found");
 					}
-				} else {
-					startActivity(i);
 				}
+
 			}
 		});
 
