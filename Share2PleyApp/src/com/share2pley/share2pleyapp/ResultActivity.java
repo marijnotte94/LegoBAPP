ddbacf814a630d36f98b03e4409170d7e75704ac
Marijn Otte <marijnotte94@gmail.com>
2015-06-05 16:59:48 +0200

    modify report

--- Share2PleyApp/src/com/share2pley/share2pleyapp/ResultActivity.java
+++ Share2PleyApp/src/com/share2pley/share2pleyapp/ResultActivity.java
@@ -1,13 +1,34 @@
 package com.share2pley.share2pleyapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultActivity extends Activity{
+	
+	private Button mExit;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result);
+		
+		mExit = (Button) findViewById(R.id.button_result_exit);
+		mExit.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				finish();
+				Intent intent = new Intent(Intent.ACTION_MAIN);
+				intent.addCategory(Intent.CATEGORY_HOME);
+				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(intent);
+			}
+		});
+		
 	}
+	
+	
 
 }
